edbfbebea1cbeb75864dd49ab473ab1d9d432601
johncrawley <johncrawley1@gmail.com>
2021-10-18 15:22:07 +0100

    Added app icon, and reduced gem glow.

--- app/src/main/java/com/jcrawleydev/gemsdrop/gem/GemPaintOptions.java
+++ app/src/main/java/com/jcrawleydev/gemsdrop/gem/GemPaintOptions.java
@@ -1,6 +1,5 @@
 package com.jcrawleydev.gemsdrop.gem;
 
-import android.content.Context;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Paint;
 
@@ -10,7 +9,7 @@ public class GemPaintOptions {
 
     public GemPaintOptions(int gemSize){
         paint = new Paint();
-        int radius = gemSize / 6;
+        int radius = gemSize / 8;
         paint.setMaskFilter(new BlurMaskFilter(radius, BlurMaskFilter.Blur.SOLID));
     }
 
