48a7ac2e0e75ad54d9deda3dfd524fc3fd538360
johncrawley <johncrawley1@gmail.com>
2021-11-02 15:30:44 +0000

    Added rotating gems decoration to the title screen.

--- app/src/main/java/com/jcrawleydev/gemsdrop/state/TitleState.java
+++ app/src/main/java/com/jcrawleydev/gemsdrop/state/TitleState.java
@@ -7,11 +7,15 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jcrawleydev.gemsdrop.Game;
 import com.jcrawleydev.gemsdrop.MainActivity;
 import com.jcrawleydev.gemsdrop.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.core.content.res.ResourcesCompat;
 
@@ -24,21 +28,52 @@ public class TitleState implements GameState {
     private final TextView tapToPlayText;
     private TranslateAnimation titleViewDisappearAnimation;
     private boolean hasClicked = false;
+    private List<ImageView> titleGems;
+    private final Animation rotateAnimation;
+    private final MainActivity activity;
 
     public TitleState(MainActivity activity, Game game, View titleView, int screenHeight){
+        this.activity = activity;
         this.game = game;
         this.titleView = titleView;
         this.screenHeight = screenHeight;
         textAnimation =  AnimationUtils.loadAnimation(activity, R.anim.text_bounce);
+        rotateAnimation =  AnimationUtils.loadAnimation(activity, R.anim.rotate_infinite);
         textAnimation.setRepeatCount(Animation.INFINITE);
         tapToPlayText = activity.findViewById(R.id.tapToPlayTextView);
-
         TextView titleTextView = activity.findViewById(R.id.titleTextView);
         Typeface customTypeface = ResourcesCompat.getFont(activity, R.font.pcalc_font);
         tapToPlayText.setTypeface(customTypeface);
         int topColor = activity.getColor(R.color.score_text_top);
         titleTextView.setTextColor(topColor);
         setupTitleViewAnimation();
+        setupTitleGems();
+    }
+
+    private void setupTitleGems(){
+        titleGems = new ArrayList<>();
+        addGemView(R.id.titleJewel4);
+        addGemView(R.id.titleJewel5);
+        addGemView(R.id.titleJewel6);
+    }
+
+
+    private void addGemView(int id){
+        titleGems.add(activity.findViewById(id));
+    }
+
+
+    private void startGemAnimation(){
+        for(View view: titleGems){
+            view.startAnimation(rotateAnimation);
+        }
+    }
+
+
+    private void stopAnimationsOnGems(){
+        for(View view: titleGems){
+            view.clearAnimation();
+        }
     }
 
 
@@ -54,19 +89,16 @@ public class TitleState implements GameState {
 
 
     public void start(){
-        System.out.println("TitleState start()");
         hasClicked = false;
         titleView.clearAnimation();
         final Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(()->{
-            tapToPlayText.startAnimation(textAnimation);
-        }, 1);
-
+        handler.postDelayed(()-> tapToPlayText.startAnimation(textAnimation), 1);
+        startGemAnimation();
     }
 
 
     public void stop(){
-
+        stopAnimationsOnGems();
     }
 
 
@@ -82,7 +114,7 @@ public class TitleState implements GameState {
             textAnimation.cancel();
             tapToPlayText.clearAnimation();
             game.loadInGameState();
-        }, 1000);
+        }, 200);
 
     }
 }
