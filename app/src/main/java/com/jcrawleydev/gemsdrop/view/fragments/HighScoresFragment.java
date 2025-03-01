74a9c968c85bc5e8a0dceeeb871c7f19e120f383
john <johncrawley1@gmail.com>
2025-03-01 23:59:06 +0000

    Created fragment for high scores.

---
+++ app/src/main/java/com/jcrawleydev/gemsdrop/view/fragments/HighScoresFragment.java
@@ -0,0 +1,46 @@
+package com.jcrawleydev.gemsdrop.view.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.jcrawleydev.gemsdrop.R;
+
+
+public class HighScoresFragment extends Fragment {
+
+    private TitleGemsAnimator titleGemsAnimator;
+
+
+    public HighScoresFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View parent = inflater.inflate(R.layout.fragment_high_scores, container, false);
+        titleGemsAnimator = new TitleGemsAnimator(parent, getContext());
+        titleGemsAnimator.start();
+        return parent;
+    }
+
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        titleGemsAnimator.stop();
+    }
+
+
+}
